ef0190715442751107c51ef0a8d30715b476dc21
cristophol <thomas.cristophol@gmail.com>
2017-05-31 12:32:09 +0200

    blog php change

--- create-file.php
+++ create-file.php
@@ -4,12 +4,12 @@
 <!--method pour recuperé des information d'un text area et les stocké dans un fichier txt qu'on creé en PHP-->
 
 <?php
-$titre = $_POST['title'];
-$text = $_POST['text'];
-$file = fopen("text.txt", "w");
-fwrite($file, $text);
-fwrite($file, $titre);
-fclose($file);
+// $titre = $_POST['title'];
+// $text = $_POST['text'];
+// $file = fopen("text.txt", "w");
+// fwrite($file, $text);
+// fwrite($file, $titre);
+// fclose($file);
 ?>
 
 
@@ -17,22 +17,26 @@ fclose($file);
 
 
 <!--methode qui recupere les information d'un text area et les stock dans un fichier a l'interieur d'un dossier creé avec PHP'-->
-
 <?php
-   if(isset($_POST['titre'])) {
-       $titre = $_POST['titre'];
-       $text = $_POST['text'];
-       fopen($titre . '.txt', 'w');
-
-       if (!is_dir('posts')) {
-           mkdir ('posts');
-       }
-       $ficier = fopen('posts/' . $titre . '.txt', 'w');
-       fwrite ('$fichier, $contenu');
-       fclose ('$fichier');
-   }else {
-       echo '<p> Formilaire vide </p>';
-   };
-
-
+/*
+*/
+  // if(isset($_POST['create'])) {
+      if(!isset($_POST['title'])
+      && !isset($_POST['text'])) {
+          echo '<p>formulaire non envoyé</p>';
+          exit(1);
+      }
+      $titre = $_POST['title'];
+      $text = $_POST['text'];
+      if(!is_dir('posts')) {
+          mkdir('posts');
+      }
+      $fichier = fopen('posts/' . $titre
+      . '.txt', 'w');
+      fwrite($fichier, $text);
+      fclose($fichier);
+      echo '<p>bravo tu as écrit un fichier</p>';
+  // }else {
+  //     echo '<p>formulaire non envoyé</p>';
+  // }
 ?>
